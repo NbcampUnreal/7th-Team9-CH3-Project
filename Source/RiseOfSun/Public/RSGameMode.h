de04cbded9ed945dc72eba8fdfd0577195aa382d
yunaj9910-alt <yunaj9910@gmail.com>
2026-02-10 18:02:48 +0900

    [초기 클래스]

---
+++ Source/RiseOfSun/Public/RSGameMode.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameMode.h"
+#include "RSGameMode.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RISEOFSUN_API ARSGameMode : public AGameMode
+{
+	GENERATED_BODY()
+	
+};
---
+++ Source/RiseOfSun/Public/RSGameState.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameState.h"
+#include "RSGameState.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class RISEOFSUN_API ARSGameState : public AGameState
+{
+	GENERATED_BODY()
+	
+};
