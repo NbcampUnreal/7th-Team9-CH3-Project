ac7df7b4952cad86a173f5c9538712966c9174ae
ssh1202 <tmdghks9812@gmail.com>
2026-02-11 20:35:05 +0900

    몬스터 AI 구현

--- Source/RiseOfSun/Public/RSMonster.h
+++ Source/RiseOfSun/Public/RSMonster.h
@@ -1,17 +1,14 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
 #pragma once
 
 #include "CoreMinimal.h"
 #include "RSCharacter.h"
 #include "RSMonster.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class RISEOFSUN_API ARSMonster : public ARSCharacter
 {
 	GENERATED_BODY()
 	
+public:
+
 };
