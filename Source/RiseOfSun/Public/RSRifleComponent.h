e4d490fa3982636c8f27f0ba1342fd0041f30218
titanaw <wjdghtmd1907@naver.com>
2026-02-11 20:27:53 +0900

    라이플 컴포넌트
    asd

--- Source/RiseOfSun/Public/RSRifleComponent.h
+++ Source/RiseOfSun/Public/RSRifleComponent.h
@@ -8,21 +8,45 @@
 
 
 UCLASS( ClassGroup=(Custom), meta=(BlueprintSpawnableComponent) )
-class RISEOFSUN_API URSRifleComponent : public UActorComponent
+class RISEOFSUN_API URSRifleComponent : public USceneComponent
 {
 	GENERATED_BODY()
 
 public:	
 	// Sets default values for this component's properties
 	URSRifleComponent();
+	//트레이스 채널
+	TEnumAsByte<ECollisionChannel> WeaponTraceChannel = ECC_Visibility;
+
+	UPROPERTY(EditAnywhere, Category = "Weapon")
+	float FireRange = 10000.0f;
+
+	UPROPERTY(EditAnywhere, Category = "Weapon")
+	float FireDebugDuration = 1.0f;
+
+	float ReloadDuration = 3.0f;
+
+	int32 AmmoInClip = 30;
+
+	struct FTimerHandle ReloadTimerHandle;
+
+	bool bIsReloading;
+
+	bool bCanFire = true;
 
 protected:
 	// Called when the game starts
 	virtual void BeginPlay() override;
 
+	
+
 public:	
 	// Called every frame
 	virtual void TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction) override;
 
-		
+	void Fire();
+
+	void Reload();
+
+	void ReloadComplete();
 };
