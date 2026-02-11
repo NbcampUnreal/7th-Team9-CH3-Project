191107be6a54874545778042ad05efe069eba225
yunaj9910-alt <yunaj9910@gmail.com>
2026-02-11 20:57:37 +0900

    플레이어 카메라 스프링암 구현

--- Source/RiseOfSun/Private/RSCharacter.cpp
+++ Source/RiseOfSun/Private/RSCharacter.cpp
@@ -1,14 +1,15 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 
 #include "RSCharacter.h"
 
-// Sets default values
+
 ARSCharacter::ARSCharacter()
 {
- 	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+ 	
 	PrimaryActorTick.bCanEverTick = true;
 
+	
 }
 
 // Called when the game starts or when spawned
@@ -18,14 +19,13 @@ void ARSCharacter::BeginPlay()
 	
 }
 
-// Called every frame
 void ARSCharacter::Tick(float DeltaTime)
 {
 	Super::Tick(DeltaTime);
 
 }
 
-// Called to bind functionality to input
+
 void ARSCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
 {
 	Super::SetupPlayerInputComponent(PlayerInputComponent);
--- Source/RiseOfSun/Public/RSPlayer.h
+++ Source/RiseOfSun/Public/RSPlayer.h
@@ -1,4 +1,4 @@
-// Fill out your copyright notice in the Description page of Project Settings.
+﻿// Fill out your copyright notice in the Description page of Project Settings.
 
 #pragma once
 
@@ -14,4 +14,25 @@ class RISEOFSUN_API ARSPlayer : public ARSCharacter
 {
 	GENERATED_BODY()
 	
+	ARSPlayer();
+
+protected:
+	virtual void BeginPlay() override;
+
+public:
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+protected:
+	UPROPERTY(EditAnywhere)
+	class USpringArmComponent* SpringArm;
+
+	UPROPERTY(EditAnywhere)
+	class UCameraComponent* Camera;
+
+private:
+	void InitializationPlayerMesh();
+
+	void InitializationPlayerCamera();
 };
