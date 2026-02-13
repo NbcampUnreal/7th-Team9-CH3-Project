a4f023995d9322a2407a9a285e19be9be5f8196d
voov-v <yumin5482@gmail.com>
2026-02-13 17:53:52 +0900

    [몬스터 데이터]

---
+++ Source/RiseOfSun/Public/MonsterSpawnRow.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "RSMonster.h"
+#include "MonsterSpawnRow.generated.h"
+
+USTRUCT(BlueprintType)
+struct FMonsterSpawnRow : public FTableRowBase
+{
+    GENERATED_BODY()
+
+public:
+    // 몬스터 고유 이름 (데이터 식별용)
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FName MonsterName;
+    // 실제로 스폰될 몬스터 클래스
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    TSubclassOf<ARSMonster> MonsterClass;
+    //몬스터 스폰 확률
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    float SpawnChance;
+};
