959ea0618bc346daa9c5e1093c1cf0d82d196502
junhee030707-pixel <junhee030707@gmail.com>
2026-02-23 02:38:37 +0900

    [아이템 제작] 헤더만 작성

---
+++ Source/RiseOfSun/Private/RSItemData.cpp
@@ -0,0 +1,12 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RSItemData.h"
+
+RSItemData::RSItemData()
+{
+}
+
+RSItemData::~RSItemData()
+{
+}
---
+++ Source/RiseOfSun/Public/RSItemData.h
@@ -0,0 +1,48 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "RSItemData.generated.h"
+
+UENUM(BlueprintType)
+enum class EItemCategory : uint8
+{
+    Weapon,
+    Consumable,
+    Material
+};
+
+UENUM(BlueprintType)
+enum class EWeaponType : uint8
+{
+    None,
+    Rifle,
+    MachineGun
+};
+
+USTRUCT(BlueprintType)
+struct FRSItemData
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FName ItemID;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    FString ItemName;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    EItemCategory Category;
+
+    // 무기일 경우
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    EWeaponType WeaponType = EWeaponType::None;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    int32 AttackPower = 0;
+
+    // 소모품 개수용
+    UPROPERTY(EditAnywhere, BlueprintReadWrite)
+    int32 StackCount = 1;
+};
