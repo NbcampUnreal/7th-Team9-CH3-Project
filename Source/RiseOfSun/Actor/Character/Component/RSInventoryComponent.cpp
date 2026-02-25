12d2d7f9f2da5f515b6613c3beca8f19c7f6f85d
rain <junhee030707@gmail.com>
2026-02-25 18:41:06 +0900

    [코드 수정] 수류탄 연동 인스턴스., 매니저, 아이템 베이스
    인벤토리와 연동 필요해 일부 수정했음 -> 기존 코드 그대로 둠

--- Source/RiseOfSun/Actor/Character/Component/RSInventoryComponent.cpp
+++ Source/RiseOfSun/Actor/Character/Component/RSInventoryComponent.cpp
@@ -8,7 +8,42 @@ URSInventoryComponent::URSInventoryComponent()
 }
 
 bool URSInventoryComponent::AddItem(URSItemBase* Item)
-{	// 인벤토리 용량 제한, 아이템인지 확인
+{	
+	//널 포인터 방지
+	if (!Item)
+		return false;
+
+	int32 RemainingStack = Item->StackCount;
+
+	// ① 기존 슬롯 먼저 채우기 (이중 for 제거)
+	for (URSItemBase* ExistingItem : Items)
+	{
+		if (!ExistingItem)
+			continue;
+
+		if (ExistingItem->GetItemID() == Item->GetItemID() &&
+			ExistingItem->IsStackable())
+		{
+			int32 MaxStack = ExistingItem->ItemData.MaxStack;
+			int32 SpaceLeft = MaxStack - ExistingItem->StackCount;
+
+			if (SpaceLeft <= 0)
+				continue;
+
+			int32 AddAmount = FMath::Min(SpaceLeft, RemainingStack);
+
+			ExistingItem->StackCount += AddAmount;
+			RemainingStack -= AddAmount;
+
+			if (RemainingStack <= 0)
+			{
+				OnInventoryUpdated.Broadcast();
+				return true;
+			}
+		}
+	}
+
+	// 인벤토리 용량 제한, 아이템인지 확인
 	if (Items.Num() >= Capacity || !Item)
 	{
 		return false;
--- Source/RiseOfSun/Actor/Item/RSGrenadeItem.cpp
+++ Source/RiseOfSun/Actor/Item/RSGrenadeItem.cpp
@@ -6,16 +6,14 @@
 
 ARSGrenadeItem::ARSGrenadeItem()
 {
-	//물리 엔진이 직접 움직이게 함.
+	PrimaryActorTick.bCanEverTick = false;
 	Mesh->SetSimulatePhysics(true);
-	Mesh->SetCollisionProfileName(TEXT("PhysicsActor")); // 충돌 성격을 물리 오브젝트로 설정
-
-	//충돌 이벤트 활성화
 	Mesh->SetNotifyRigidBodyCollision(true);
-	Mesh->SetEnableGravity(true);
+	Mesh->SetCollisionEnabled(ECollisionEnabled::QueryAndPhysics);
+	Mesh->SetCollisionObjectType(ECC_PhysicsBody);
+	Mesh->SetCollisionResponseToAllChannels(ECR_Block);
 	Mesh->SetLinearDamping(0.2f);
 	Mesh->SetAngularDamping(0.1f);
-
 	Mesh->OnComponentHit.AddDynamic(this, &ARSGrenadeItem::OnHit);
 }
 
@@ -29,7 +27,7 @@ void ARSGrenadeItem::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPr
 		// 반경 데미지 (예시)
 		UGameplayStatics::ApplyRadialDamage(
 			this,
-			100.f,                         // 데미지
+			ItemData.AttackPower,                        // 데미지
 			GetActorLocation(),            // 위치
 			300.f,                         // 반경
 			nullptr, // 어떤 종류의 데미지인지 지정하는 자리
@@ -38,6 +36,24 @@ void ARSGrenadeItem::OnHit(UPrimitiveComponent* HitComp, AActor* OtherActor, UPr
 			GetInstigatorController(), //주체(소유자)
 			true
 		);
+
+		if (ExplosionEffect)
+		{
+			UGameplayStatics::SpawnEmitterAtLocation(
+				GetWorld(),
+				ExplosionEffect,
+				GetActorLocation()
+			);
+		}
+		if (ExplosionSound)
+		{
+			UGameplayStatics::PlaySoundAtLocation(
+				this,
+				ExplosionSound,
+				GetActorLocation()
+			);
+		}
+
 
 		Destroy();
 	}
